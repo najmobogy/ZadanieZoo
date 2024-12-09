92bea9d7426dcf8df1031ce5ba36759ffdff948f
Bog <najmobogi@gmail.com>
2024-12-09 10:02:48 +0100

    Commit

--- app.js
+++ app.js
@@ -6,12 +6,12 @@ const port = process.env.PORT || 3000;
 
 app.use(express.json());
 
-// Endpoints
+
 app.get("/animals", animalsController.getAllAnimals);
 app.get("/animals/:id", animalsController.getAnimalById);
 app.get("/animals/endangered", animalsController.getEndangeredAnimals);
 app.get("/animals/habitat/:habitat", animalsController.getAnimalsByHabitat);
-app.get("/animals/species", animalsController.getAnimalsBySpecies); // Query param
+app.get("/animals/species", animalsController.getAnimalsBySpecies);
 app.post("/animals", animalsController.addAnimal);
 app.put("/animals/:id", animalsController.updateAnimal);
 app.delete("/animals/:id", animalsController.deleteAnimal);
